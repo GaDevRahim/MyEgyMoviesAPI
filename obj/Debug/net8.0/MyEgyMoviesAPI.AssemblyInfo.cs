5797611829b26d8129766de36117fc77f807a6ab
Mahmoud Rahim <103900403+GaDevRahim@users.noreply.github.com>
2024-03-23 06:21:41 +0200

    Bulid basics of Project

--- obj/Debug/net8.0/MyEgyMoviesAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyEgyMoviesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyEgyMoviesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da82139c08f50e20af886b927620f11720599dad")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyEgyMoviesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyEgyMoviesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
